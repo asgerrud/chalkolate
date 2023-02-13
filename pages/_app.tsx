a5a3297a8e9335e9cded084571f1067f1a4bf079
asgerrud <asgerrud@gmail.com>
2023-02-13 23:30:34 +0100

    Setup ESLint and Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
-import { AppProps } from 'next/app';
+import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
   // Create a new supabase browser client on every first render.
