7f6631bf2421cea2d89bb3ae2a8c61919c80cf8f
asgerrud <asgerrud@gmail.com>
2023-02-13 23:40:25 +0100

    chore: update ESLint rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,23 @@
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
-import { AppProps } from 'next/app'
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Session, SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) {
   // Create a new supabase browser client on every first render.
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
   return (
-      <SessionContextProvider
-          supabaseClient={supabaseClient}
-          initialSession={pageProps.initialSession}
-      >
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <ChakraProvider>
         <Component {...pageProps} />
-      </SessionContextProvider>
-  )
+      </ChakraProvider>
+    </SessionContextProvider>
+  );
 }
