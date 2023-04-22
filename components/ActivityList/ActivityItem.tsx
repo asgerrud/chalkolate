a91776529176e5e415ebc3b6bc844ba84eb31e59
asgerrud <asgerrud@gmail.com>
2023-04-22 22:31:24 +0200

    Create ActivityItem component

--- components/ActivityList/ActivityItem.tsx
+++ components/ActivityList/ActivityItem.tsx
@@ -1,28 +1,68 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  HStack,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Text
+} from "@chakra-ui/react";
 import { Activity, ClimbingLocation } from "@/types/database";
 import { FC } from "react";
 import { useHydrationSafeDate } from "@/hooks/use-hydration-safe-date";
+import { X } from "lucide-react";
 
 type Props = {
   activity: Activity;
   locations: ClimbingLocation[];
+  onRemoveActivity: (activityId: string) => void;
 };
 
-export const ActivityItem: FC<Props> = ({ activity, locations }) => {
+export const ActivityItem: FC<Props> = ({ activity, locations, onRemoveActivity }) => {
   const formattedDate = useHydrationSafeDate(activity.activity_date);
+
   const getLocationName = (id: string): string => {
-    const location = locations.find(
-      (location: ClimbingLocation) => location.id === id,
-    );
+    const location = locations.find((location: ClimbingLocation) => location.id === id);
     return location?.name || null;
   };
 
+  const removeActivity = () => {
+    onRemoveActivity(activity.id);
+  };
+
   return (
-    <>
-      <Text>{formattedDate}</Text>
-      {getLocationName(activity.location) && (
-        <Text>{getLocationName(activity.location)}</Text>
-      )}
-    </>
+    <Flex key={activity.id} w="100%" justifyContent="space-between" alignItems="center" px={3} py={2} bg="gray.100">
+      <HStack spacing={4}>
+        <Text>{formattedDate}</Text>
+        {getLocationName(activity.location) && <Text>{getLocationName(activity.location)}</Text>}
+      </HStack>
+      <HStack spacing={4}>
+        <Text>{activity.duration}</Text>
+        <Popover>
+          <PopoverTrigger>
+            <Button size="xs" variant="unstyled">
+              <X></X>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverHeader>Delete activity</PopoverHeader>
+            <PopoverBody>
+              Are you sure you want to delete the activity?
+              <Flex justifyContent="flex-end">
+                <Button colorScheme="red" onClick={() => removeActivity()}>
+                  Delete
+                </Button>
+              </Flex>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </HStack>
+    </Flex>
   );
 };
--- components/ActivityList/ActivityList.tsx
+++ components/ActivityList/ActivityList.tsx
@@ -1,28 +1,8 @@
-import {
-  Box,
-  ButtonGroup,
-  Flex,
-  HStack,
-  List,
-  ListItem,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
-  Skeleton,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
 import AddActivity from "@/components/modals/AddActivity";
-import { Activity, ClimbingLocation } from "@/types/database";
-import { FC, Suspense, useState } from "react";
-import { Button } from "@chakra-ui/button";
-import { X } from "lucide-react";
 import { supabase } from "@/lib/supabase";
-import { useHydrationSafeDate } from "../../hooks/use-hydration-safe-date";
+import { Activity, ClimbingLocation } from "@/types/database";
+import { List, ListItem } from "@chakra-ui/react";
+import { FC, useState } from "react";
 import { ActivityItem } from "./ActivityItem";
 
 type Props = {
@@ -43,9 +23,7 @@ const ActivityList: FC<Props> = ({ initialActivities, locations }) => {
       alert(error.message);
     }
 
-    setActivities(
-      activities.filter((activity: Activity) => activity.id !== id),
-    );
+    setActivities(activities.filter((activity: Activity) => activity.id !== id));
   };
 
   return (
@@ -54,45 +32,7 @@ const ActivityList: FC<Props> = ({ initialActivities, locations }) => {
       <List spacing={3}>
         <ListItem>Show list of activities</ListItem>
         {activities.map((activity: Activity) => {
-          return (
-            <Flex
-              key={activity.id}
-              w="100%"
-              justifyContent="space-between"
-              alignItems="center"
-              px={3}
-              py={2}
-              bg="gray.100">
-              <HStack spacing={4}>
-                <ActivityItem activity={activity} locations={locations} />
-              </HStack>
-              <HStack spacing={4}>
-                <Text>{activity.duration}</Text>
-                <Popover>
-                  <PopoverTrigger>
-                    <Button size="xs" variant="unstyled">
-                      <X></X>
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent>
-                    <PopoverArrow />
-                    <PopoverCloseButton />
-                    <PopoverHeader>Delete activity</PopoverHeader>
-                    <PopoverBody>
-                      Are you sure you want to delete the activity?
-                      <Flex justifyContent="flex-end">
-                        <Button
-                          colorScheme="red"
-                          onClick={() => removeActivity(activity.id)}>
-                          Delete
-                        </Button>
-                      </Flex>
-                    </PopoverBody>
-                  </PopoverContent>
-                </Popover>
-              </HStack>
-            </Flex>
-          );
+          return <ActivityItem key={activity.id} activity={activity} locations={locations} onRemoveActivity={removeActivity} />;
         })}
       </List>
     </>
