210e63c26ba08fde53d74e3f089c470c27692e56
Asger Rud Hansen <asgerrud@gmail.com>
2023-05-07 14:43:50 +0200

    Add tentative primary hover color

--- theme.ts
+++ theme.ts
@@ -5,20 +5,21 @@ const extendedTheme = extendTheme(
     colors: {
       primary: {
         500: "#20b887",
+        600: "#118968"
       },
       white: "#FFFFFF",
       background: "#f7f7f7",
       lightGreen: "#e0eeee",
-      black: "#1e293a",
+      black: "#1e293a"
     },
     config: {
       initialColorMode: "light",
-      useSystemColorMode: false,
-    },
+      useSystemColorMode: false
+    }
   },
   withDefaultColorScheme({
-    colorScheme: "primary",
-  }),
+    colorScheme: "primary"
+  })
 );
 
 export default extendedTheme;
