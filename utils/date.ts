dc3f8d491f80b594b670861df65e53993d02215e
asgerrud <asgerrud@gmail.com>
2023-05-18 16:55:48 +0200

    Refactor date functions

--- utils/date.ts
+++ utils/date.ts
@@ -1,35 +1,32 @@
-const WEEK_IN_HOURS = 24 * 7;
+const MILISECONDS_TO_WEEKS = 1000 * 60 * 60 * 24 * 7;
 
-export const hoursBetweenDates = (a: Date, b: Date) => {
+export const hoursBetweenDates = (a: Date, b: Date): number => {
   const msBetweenDates = Math.abs(a.getTime() - b.getTime());
   const milisecondToHourMultiplier = 60 * 60 * 1000;
   return msBetweenDates / milisecondToHourMultiplier;
 };
 
-const getDaysFromMonday = (date: Date): number => {
-  const day = date.getDay();
-  return day === 0 ? 6 : day - 1;
-};
-
-const setBeginningOfWeek = (dates: Date[]): void => {
-  for (let date of dates) {
-    if (date.getUTCHours() > 12) {
-      date.setUTCHours(24, 0, 0, 0);
-    } else {
-      date.setUTCHours(0, 0, 0, 0);
-    }
-    date.setDate(date.getDate() - getDaysFromMonday(date));
-  }
+const getDateBeginningOfWeek = (date: Date): Date => {
+  const newDate = new Date(date);
+  newDate.setHours(0, 0, 0, 0);
+  newDate.setDate(newDate.getDate() - newDate.getDay());
+  return newDate;
 };
 
 export const isDateInSameWeek = (a: Date, b: Date): boolean => {
-  setBeginningOfWeek([a, b]);
-  return hoursBetweenDates(a, b) === 0;
+  const d1 = getDateBeginningOfWeek(a);
+  const d2 = getDateBeginningOfWeek(b);
+
+  return d1.getTime() === d2.getTime();
 };
 
 export const isDateInAdjacentWeek = (a: Date, b: Date): boolean => {
-  setBeginningOfWeek([a, b]);
-  return hoursBetweenDates(a, b) > 0 && hoursBetweenDates(a, b) <= WEEK_IN_HOURS;
+  const d1 = getDateBeginningOfWeek(a);
+  const d2 = getDateBeginningOfWeek(b);
+
+  const weekDiff = Math.abs((d2.getTime() - d1.getTime()) / MILISECONDS_TO_WEEKS);
+
+  return weekDiff === 1;
 };
 
 export const getTodaysDate = (): Date => {
@@ -38,6 +35,6 @@ export const getTodaysDate = (): Date => {
   return new Date(today.toISOString());
 };
 
-export const compareDates = (a: string, b: string) => {
+export const compareDates = (a: string, b: string): number => {
   return Date.parse(a) - Date.parse(b);
 };
