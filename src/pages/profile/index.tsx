80b2d95e189f9271133f23aa928c5f9f32858dd5
asgerrud <asgerrud@gmail.com>
2023-07-02 23:16:48 +0200

    Create HOC for route protection

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,60 +1,12 @@
 import Layout from "~/components/Layout";
-import { Stack } from "@chakra-ui/react";
-import ChallengeCard from "~/components/profile/challenge-card/ChallengeCard";
-import ActivityCard from "~/components/profile/activity-card/ActivityCard";
-import { type Activity } from ".prisma/client";
+import { requireAuth } from "~/utils/requireAuth";
 
-interface ProfilePageProps {
-  activities: Activity[];
-  locations: ClimbingLocation[];
-  climbingZones: ClimbingZone[];
-  changeSchedules: ChangeSchedule[];
-  techniques: Technique[];
-  grades: Grade[];
+export function ProfilePage() {
+  return <Layout>Profile</Layout>;
 }
 
-export function ProfilePage({
-  activities,
-  locations,
-  climbingZones,
-  changeSchedules,
-  techniques,
-  grades
-}: ProfilePageProps) {
-  return (
-    <Layout>
-      <Stack w="100%" direction="column" alignItems="center" spacing={6}>
-        <ActivityCard activities={activities} locations={locations} />
-        <ChallengeCard
-          locations={locations}
-          climbingZones={climbingZones}
-          changeSchedules={changeSchedules}
-          techniques={techniques}
-          grades={grades}
-        />
-      </Stack>
-    </Layout>
-  );
-}
-
-export async function getStaticProps() {
-  const locations: ClimbingLocation[] = await fetchLocations();
-  const activities: Activity[] = await fetchActivities();
-  const climbingZones: ClimbingZone[] = await fetchClimbingZones();
-  const changeSchedules: ChangeSchedule[] = await fetchChangeSchedules();
-  const techniques: Technique[] = await fetchTechniques();
-  const grades: Grade[] = await fetchGrades();
-
-  return {
-    props: {
-      activities,
-      locations,
-      climbingZones,
-      changeSchedules,
-      techniques,
-      grades
-    }
-  };
-}
+export const getServerSideProps = requireAuth(() => {
+  return { props: {} };
+});
 
 export default ProfilePage;
