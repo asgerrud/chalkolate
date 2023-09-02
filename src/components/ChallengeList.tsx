7c03c36b378295e900990c897c8ef6fa28d7c6a8
asgerrud <asgerrud@gmail.com>
2023-09-02 21:06:16 +0200

    Add challenge list component

---
+++ src/components/ChallengeList.tsx
@@ -0,0 +1,63 @@
+import { api } from "~/utils/api";
+import { Box, Card, CardBody, Center, Flex, Heading, Spinner, Stack, Text } from "@chakra-ui/react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { NewChallengeForm } from "~/components/NewChallengeForm";
+
+dayjs.extend(relativeTime);
+
+function NoChallengesFound() {
+  return <Text>No challenges found</Text>;
+}
+
+export default function ChallengeList() {
+  const challenges = api.challenge.findUserChallenges.useQuery({});
+
+  const hasChallenges = challenges.data?.length;
+
+  if (challenges.isLoading) {
+    return (
+      <Center>
+        <Spinner />
+      </Center>
+    );
+  }
+
+  return (
+    <Box p={4} my={2} bgColor="lightGreen">
+      <Heading size="md" mb={4}>
+        Challenges
+      </Heading>
+      <Stack spacing={4}>
+        {hasChallenges ? (
+          <Stack spacing={4}>
+            {challenges?.data?.map((challenge) => {
+              const { id, grade, location, zone, endDate } = challenge;
+              const timeToChallengeEnd = dayjs(endDate).fromNow(true);
+
+              return (
+                <Card key={id}>
+                  <CardBody>
+                    <Flex justifyContent="space-between">
+                      <Stack>
+                        <Text fontWeight="bold">{location.name}</Text>
+                        <Text>{zone.name}</Text>
+                      </Stack>
+                      <Stack textAlign="end">
+                        <Text>{timeToChallengeEnd} remaining</Text>
+                        <Text>{grade.name}</Text>
+                      </Stack>
+                    </Flex>
+                  </CardBody>
+                </Card>
+              );
+            })}
+          </Stack>
+        ) : (
+          <NoChallengesFound />
+        )}
+        <NewChallengeForm />
+      </Stack>
+    </Box>
+  );
+}
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,11 +1,14 @@
 import Layout from "~/components/Layout";
 import { requireAuth } from "~/utils/requireAuth";
-import { NewChallengeForm } from "~/components/NewChallengeForm";
+import ChallengeList from "~/components/ChallengeList";
+import { Stack } from "@chakra-ui/react";
 
 export function ProfilePage() {
   return (
     <Layout>
-      <NewChallengeForm />
+      <Stack maxW="480px" w="100%" px={6}>
+        <ChallengeList />
+      </Stack>
     </Layout>
   );
 }
