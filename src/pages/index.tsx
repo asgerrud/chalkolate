93816cfd9c2f36d7aa0489c8cd4d1266a6d37f0e
asgerrud <asgerrud@gmail.com>
2023-09-02 22:25:18 +0200

    Migrate landing page to shadcn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,30 @@
 import Layout from "~/components/Layout";
-import { Button, Center, Stack, Text } from "@chakra-ui/react";
 import { getProviders, signIn } from "next-auth/react";
 import { type GetServerSidePropsContext, type InferGetServerSidePropsType } from "next";
 import { RiDiscordFill, RiGoogleFill } from "react-icons/ri";
 import { getServerSession } from "next-auth";
 import { authOptions } from "~/server/auth";
+import { Button } from "~/components/ui/button";
 
 export default function Home({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { discord, google } = providers;
 
   return (
     <Layout pageTitle="Home" navbarDisabled>
-      <Center flexDirection="column" maxW="320px">
-        <Text fontSize="xl" mb={16}>
-          Start tracking your bouldering skills and develop like a pro
-        </Text>
-        <Stack w="100%" spacing={4}>
-          <Button mx="auto" w="100%" onClick={() => signIn()}>
-            Login
-          </Button>
-          <Button variant="outline" colorScheme="black" onClick={() => signIn(google.id)}>
+      <div className="flex flex-col max-w-[320px]">
+        <p className="text-xl mb-10">Start tracking your bouldering skills and develop like a pro</p>
+        <div className="flex flex-col w-full space-y-2">
+          <Button onClick={() => signIn()}>Login</Button>
+          <Button variant="outline" onClick={() => signIn(google.id)}>
             <RiGoogleFill size="1rem" />
-            <Text ml={1}>Sign in with {google.name}</Text>
+            <p className="ml-1">Sign in with {google.name}</p>
           </Button>
-          <Button variant="outline" colorScheme="black" onClick={() => signIn(discord.id)}>
+          <Button variant="outline" onClick={() => signIn(discord.id)}>
             <RiDiscordFill size="1rem" />
-            <Text ml={1}>Sign in with {discord.name}</Text>
+            <p className="ml-1">Sign in with {discord.name}</p>
           </Button>
-        </Stack>
-      </Center>
+        </div>
+      </div>
     </Layout>
   );
 }
@@ -43,6 +39,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const providers = await getProviders();
 
   return {
-    props: { providers: providers ?? [] }
+    props: { providers }
   };
 }
