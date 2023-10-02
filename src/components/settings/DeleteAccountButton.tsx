674c4011cff80cc3a644e74bd77b2c6a25c97c8f
asgerrud <asgerrud@gmail.com>
2023-10-02 20:52:27 +0200

    Create delete account button dialog component

---
+++ src/components/settings/DeleteAccountButton.tsx
@@ -0,0 +1,34 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "~/components/ui/alert-dialog";
+import { Button } from "~/components/ui/button";
+
+export function DeleteAccountButton() {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Delete account</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your account and remove your data
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction variant="destructive">Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,14 +1,17 @@
 import Layout from "~/components/Layout";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { DeleteAccountButton } from "~/components/settings/DeleteAccountButton";
 
 export function SettingsPage() {
   return (
     <Layout>
       <Card className="max-w-[480px] w-full px-6">
         <CardHeader>
-          <CardTitle>Challenges</CardTitle>
+          <CardTitle>Account Settings</CardTitle>
         </CardHeader>
-        <CardContent>Delete account</CardContent>
+        <CardContent>
+          <DeleteAccountButton />
+        </CardContent>
       </Card>
     </Layout>
   );
